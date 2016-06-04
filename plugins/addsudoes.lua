e0e5eb4bd6fa05251b63facbd3753fef46ef3da8
ddimon <dimon3star@gmail.com>
2016-06-04 16:55:41 +0430

    Update addsudoes.lua

--- plugins/addsudoes.lua
+++ plugins/addsudoes.lua
@@ -4,7 +4,7 @@ vardump(success)
 vardump(result)
 end
 local function run(msg, matches)
-local sudo = 85831686 --Put you id Here !
+local sudo = 203079836 --Put you id Here !
 local addsudo = 'user#id'..sudo
 local chat = get_receiver(msg)
 if is_momod(msg) then -- you can set it to is_owner(msg)
@@ -15,8 +15,8 @@ return {
 description = "Add my sudo with you",
 usage = {
 user = {
-" List Help For Sphero-Bot:",
-"Developed by : @Joker_admin_1",
+" List Help For DiMoM-bot:",
+"Developed by : @xxxdimonxxx",
 },
 moderator = {
 "!addsudo : invite Sudo to your group" },
