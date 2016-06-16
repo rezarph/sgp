bf112551aac5ea9b143b2c518b64b60043aef30b
alomina007 <alomina007@users.noreply.github.com>
2016-06-16 07:38:23 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -113,5 +113,5 @@ else
     exit 1
   fi
 
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/alomina.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/firebot.lua -l 1 -E $@
 fi
